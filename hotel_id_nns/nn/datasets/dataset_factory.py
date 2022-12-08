25e8a51b6ff4bd6b0c365232ce57ea5bd966ea4c
Christopher von Klitzing <christopherklitzing@gmail.com>
2022-12-08 14:58:23 +0100

    Modify DatasetFactory to use csv files as default, if h5 files are not available

--- hotel_id_nns/nn/datasets/dataset_factory.py
+++ hotel_id_nns/nn/datasets/dataset_factory.py
@@ -8,9 +8,17 @@ from hotel_id_nns.nn.datasets.chain_dataset_h5 import H5ChainDataset
 class DatasetFactory(object):
 
     def get(self, path: Path, config: Dict) -> Union[H5ChainDataset, ChainDataset]:
-        if path.suffix == '.h5':
-            return H5ChainDataset(annotations_file_path=path, config=config)
-        elif path.suffix == '.csv':
-            return ChainDataset(annotations_file_path=path, config=config)
+        actual_path = path
+
+        # A default-switch which changes to the slower csv dataset instead of the faster h5 because
+        # the annotation files for h5 dataset-loading are missing.
+        if path.suffix == '.h5' and not path.exists():
+            actual_path = path.with_suffix('.csv')
+            print("File '" + str(path) + "' not found. Use .csv suffix instead")
+
+        if actual_path.suffix == '.h5':
+            return H5ChainDataset(annotations_file_path=actual_path, config=config)
+        elif actual_path.suffix == '.csv':
+            return ChainDataset(annotations_file_path=actual_path, config=config)
         else:
             raise NotImplementedError()
