3819992f16e00323eee91c4ef8b8ec674596bf1b
Claudius Kienle <privat@claudiuskienle.de>
2022-12-09 15:35:38 +0100

    added cm to wandb

--- hotel_id_nns/nn/trainers/chain_id_trainer.py
+++ hotel_id_nns/nn/trainers/chain_id_trainer.py
@@ -79,7 +79,7 @@ class ChainIDTrainer(Trainer):
         }
 
         if detailed_info:
-            info['cm'] = confusion_matrix(pred_chain_id_probs, chain_ids)
+            info['cm'] = confusion_matrix(pred_chain_id_probs.cpu().detach(), chain_ids.cpu().detach())
 
         return loss, info
 
