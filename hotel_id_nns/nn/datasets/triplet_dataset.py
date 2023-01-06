9c1a56134c0b3e8b0bed918ca3e6a6a38067145b
Claudius Kienle <privat@claudiuskienle.de>
2023-01-06 13:51:35 +0100

    improve triplet dataset

--- hotel_id_nns/nn/datasets/triplet_dataset.py
+++ hotel_id_nns/nn/datasets/triplet_dataset.py
@@ -17,14 +17,18 @@ class TripletDataset(H5HotelDataset):
         pos_anchors = (self.chain_ids == chain_id).nonzero()
         assert len(pos_anchors) > 1, 'atleast another positive sample'
         rand_index = index
-        while rand_index != index:
-            rand_index = pos_anchors[torch.randint(high=len(pos_anchors), size=(1,)).item()]
+        while rand_index == index:
+            rand_index = pos_anchors[torch.randint(high=len(pos_anchors), size=(1,))]
         positive_anchor = self.preprocess(torch.as_tensor(self.imgs[rand_index]))
         
         # negative
-        neg_anchors = (self.chain_ids != chain_id).nonzero()
+        ## sample chain id, pick random sample with that chain id
+        neg_chain_id = chain_id
+        while neg_chain_id == chain_id:
+            neg_chain_id = torch.randint(high=self.chain_ids.max(), size=(1,))
+        neg_anchors = (self.chain_ids == neg_chain_id).nonzero()
         assert len(neg_anchors) >= 1, 'at least one negative sample'
-        rand_index = neg_anchors[torch.randint(high=len(neg_anchors), size=(1,)).item()]
+        rand_index = neg_anchors[torch.randint(high=len(neg_anchors), size=(1,))]
         negative_anchor = self.preprocess(torch.as_tensor(self.imgs[rand_index]))
 
         return img, positive_anchor, negative_anchor
