0de40d2973466b1cb9c67437d0f8825a1b19e7c4
Claudius Kienle <privat@claudiuskienle.de>
2023-01-06 13:44:49 +0100

    added tripled dataset

---
+++ hotel_id_nns/nn/datasets/triplet_dataset.py
@@ -0,0 +1,32 @@
+from pathlib import Path
+from typing import Tuple
+
+import torch
+from hotel_id_nns.nn.datasets.h5_hotel_dataset import H5HotelDataset
+
+
+class TripletDataset(H5HotelDataset):
+
+    def __init__(self, annotations_file_path: Path, config: dict) -> None:
+        super().__init__(annotations_file_path, config)
+
+    def __getitem__(self, index) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        img, chain_id, hotel_id = super().__getitem__(index)
+
+        # positive
+        pos_anchors = (self.chain_ids == chain_id).nonzero()
+        assert len(pos_anchors) > 1, 'atleast another positive sample'
+        rand_index = index
+        while rand_index != index:
+            rand_index = pos_anchors[torch.randint(high=len(pos_anchors), size=(1,)).item()]
+        positive_anchor = self.preprocess(torch.as_tensor(self.imgs[rand_index]))
+        
+        # negative
+        neg_anchors = (self.chain_ids != chain_id).nonzero()
+        assert len(neg_anchors) >= 1, 'at least one negative sample'
+        rand_index = neg_anchors[torch.randint(high=len(neg_anchors), size=(1,)).item()]
+        negative_anchor = self.preprocess(torch.as_tensor(self.imgs[rand_index]))
+
+        return img, positive_anchor, negative_anchor
+
+
